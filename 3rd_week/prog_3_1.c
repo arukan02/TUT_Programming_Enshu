28f199dceea39b4e9599ff6b39f4e679a6c03862
arukan02 <alcanderimawan@gmail.com>
2024-05-13 17:48:20 +0900

    working prog_3_1

--- 3rd_week/prog_3_1.c
+++ 3rd_week/prog_3_1.c
@@ -1,31 +1,125 @@
 /* header files */
 #include <stdio.h>
+#include <stdlib.h>
 
-//Macro, define constant
-#define ARRAY_SIZE 46
+/* define */
+#define ARRAY_MAX 5
+#define STR_MAX 256
 
-/* functions */
-int fibonacci(int fibo[], int n); // 関数宣言
+/* structure */
+typedef struct queue {
+  int array[ARRAY_MAX];
+  int front;
+  int end;
+} Queue;
+
+/* declearation functions */
+void enqueue(Queue *x);
+void dequeue(Queue *x);
+void print_queue(Queue *x);
+int find_next(int current); /* For Assignment 3.1 */
+
+/* for debug */
+void print_detailed_queue(Queue *x);
 
 /* main */
 int main(void) {
-int n;
-int fibo[ARRAY_SIZE] = {0,1,1}; //initialized by 0
-printf("Fibonacci number f(n) = f(n - 1) + f(n - 2).\n");
-printf("Enter a positive integer n (1 <= n <= 45): ");
-scanf("%d", &n);
-printf("f(%d) = %d\n", n, fibonacci(fibo, n));
-return 0;
+  /* Variables */
+  Queue x;
+  int num;
+  char str[STR_MAX];
+
+  /* Initial settings */
+  x.front = 0;
+  x.end = 0;
+
+  /* Main loop */
+  while(1) { /* Infinite loop */
+    puts("Enter the operation number.");
+    printf("1. Enqueue, 2. Dequeue, 3. Print, 4. Exit: ");
+    fgets(str, sizeof(str), stdin);
+    num = atoi(str);
+    // selected operation for Queue x
+    switch(num) {
+    case 1: enqueue(&x);
+      break;
+    case 2: dequeue(&x);
+      break;
+    case 3: print_queue(&x);
+      break;
+    case 4: exit(0);
+    case 9: print_detailed_queue(&x); /* not explained */
+      break;
+    default: printf("-- You input wrong key [%d] --", num);
+    }
+  }
+  return 1;
 }
 
-/*exponential function using for loop*/
-int fibonacci(int fibo[], int n) {
-    if (n==1 || n==2){
-        return fibo[n];
-    } else if (fibo[n] != 0) { // Check if the value is already calculated and stored
-        return fibo[n];
-    } else {
-        fibo[n] = fibonacci(fibo, n - 1) + fibonacci(fibo, n - 2);
-        return fibo[n];
+/* enqueue */
+void enqueue(Queue *x) {
+  char str[STR_MAX];
+  if (find_next(x->end) == x->front) {
+    puts("-- No resource remained. --");
+  } else {
+    printf("Input a number to enqueue: ");
+    fgets(str, sizeof(str), stdin);
+    x->array[x->end] = atoi(str);
+    x->end = find_next(x->end);
+  }
+}
+
+/* dequeue */
+void dequeue(Queue *x) {
+  if (x->front == x->end) {
+    puts("-- There is no data. --");
+  } else {
+    printf("Dequeue: %d\n", x->array[x->front]);
+    x->front = find_next(x->front);
+  }
+}
+
+/* print all data */
+void print_queue(Queue *x) {
+  int i;
+  if (x->front == x->end) {
+    puts("-- There is no data. --");
+  } else {
+    while (1){
+        if(x->front == x->end) {
+            break;
+        }
+        else {
+            printf("%d ",x->array[x->front]);
+            x->front = find_next(x->front);
+        }
     }
+    putchar('\n');
+  }
+}
+
+int find_next(int current){
+    if (current == ARRAY_MAX - 1){
+        return 0;
+    }
+    else {
+        return current + 1;
+    }
+}
+
+/* print all data (detailed) */
+void print_detailed_queue(Queue *x) {
+  int i;
+  for (i = 0; i < ARRAY_MAX; i++) {
+    if ((x->front < x->end) && (x->front <= i) && (i < x->end)) {
+      /* 終端に到達していない時 */
+      printf("%3.3d : %3.3d : %d\n", i, i - x->front, x->array[i]); 
+    } else if ((x->front > x->end) && (x->front >= i) && (x->end < i)) {
+      /* 終端を超えた時 */
+      printf("%3.3d : %3.3d : %d\n", i, i - x->end, x->array[i]);
+    } else {
+      /* No data */
+      printf("%3.3d : ***\n", i);
+    }
+  }
 }
--- 3rd_week/prog_3_2.c
+++ 3rd_week/prog_3_2.c
@@ -1,32 +1,83 @@
+/* header files */
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/* define */
+#define MAX 10
+/* structure */
+typedef struct stack {
+  double array[MAX];
+  int counter;
+} Stack;
+/* functions */
+void read_rpn(void);
+void push(Stack *x, double n);
+double pop(Stack *x);
 
-/* Function declarations */
-void factorize(int n, int divisor);
-
-/* Main function */
+/* main */
 int main(void) {
-    int n;
-    printf("Prime factorization.\n");
-    printf("Enter a positive integer n (2 <= n <= 10000):  ");
-    scanf("%d", &n);
-    printf("%d = ", n);
-    factorize(n,2);
-    return 0;
+  read_rpn();
+  return 0;
 }
 
-/* Recursive function to factorize with prime number */
-void factorize(int n, int divisor) {
-    if(n == 1){
-        return;
-    }
-    if (n % divisor == 0) {
-        if (n != divisor) {
-            printf("%d x ", divisor); // Print the factor with "x" separator
+/* four basic arithmetic operations on positive real numbers */
+void read_rpn(void) {
+  Stack x;
+  int ch;
+  double num1, num2;
+  x.counter = 0;
+  printf("Enter an equation in Reverse Polish Notation: ");
+  while((ch = getchar()) != EOF) {
+    if (isdigit(ch)) { // the token is a digit
+      // convert a character code to a number
+      ungetc(ch, stdin);
+      scanf("%lf", &num1);
+      push(&x, num1);
+    } else { // the token is not a digit
+      switch(ch) {
+      case '+':
+        num2 = pop(&x); num1 = pop(&x);
+        push(&x, num1 + num2);
+        break;
+      case '-':
+        num2 = pop(&x); num1 = pop(&x);
+        push(&x, num1 - num2);
+        break;
+      case '*':
+        num2 = pop(&x); num1 = pop(&x);
+        push(&x, num1 * num2);
+        break;
+      case '/':
+        num2 = pop(&x); num1 = pop(&x);
+        push(&x, num1 / num2);
+        break;
+      case ' ': break;
+      case '\n':
+        if (x.counter <= 1) {
+          printf("Answer: %.15f\n", pop(&x));
+          exit(0);
         } else {
-            printf("%d", divisor); // Print the last factor without "x" separator
+          puts("Stack is not empty.");
+          exit(1);
         }
-        factorize(n / divisor, divisor); 
-    } else {
-        factorize(n, divisor + 1); 
+      default:
+        puts("Wrong characters entered.");
+        exit(1);
+      }
     }
+  }
+}
+
+void push(Stack *x, double n)
+{
+  /* Assignment 3.2,
+     you can write code here. 
+     x->num1 something hahahhaa*/
+}
+
+double pop(Stack *x)
+{
+  /* Assignment 3.2,
+     you can write code here. */
+  return 0.0;
 }
