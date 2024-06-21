a3a01a83ad4bf0891fb1b1515a066039ff8f4ee8
arukan02 <alcanderimawan@gmail.com>
2024-06-21 13:58:06 +0900

    doing kadai8

--- 8th_week/prog_8_1.c
+++ 8th_week/prog_8_1.c
@@ -5,7 +5,7 @@
 #define MAX 15
 
 /* functions */
-int route( int v, int h);
+int route( int v, int h, int save[MAX][MAX]);
 
 /* main */
 int main(void)
@@ -16,15 +16,27 @@ int main(void)
   scanf ("%d", &v);
   printf("Enter the number of path in the horizontal axis h (1 <= h <= %d): ", MAX);
   scanf ("%d", &h);
-  printf("The answer is %d.\n", route(v - 1, h - 1));
+
+  //initialize the save array
+  int save[MAX][MAX];
+  for(int i = 0; i < MAX; ++i)
+    for(int j = 0; j < MAX; ++j)
+      save[i][j] = -1;
+  
+  printf("The answer is %d.\n", route(v - 1, h - 1, save));
   return 0;
 }
 
 /* full search */
-int route( int v, int h) {
+int route( int v, int h, int save[MAX][MAX]) {
   if (v == 0 || h == 0) {
     return 1;
-  } else {
-    return route(v - 1, h) + route(v, h - 1);
+  } 
+  if(save[v][h] != -1) {
+    return save[v][h];
   }
+
+  //search for save if not found
+  save[v][h] = route(v - 1, h, save) + route(v, h - 1, save);
+  return save[v][h];
 }
